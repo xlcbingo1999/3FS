03d9be2915ca6b8b7b5a70a69f64c5a93ae3a9de
xlcbingo1999 <1220368815@qq.com>
2025-03-02 23:27:17 +0800

    fix: if kernel have not <linux/time_types.h>, define some var

--- third_party/liburing-cmake/liburing/compat.h
+++ third_party/liburing-cmake/liburing/compat.h
@@ -2,9 +2,24 @@
 #ifndef LIBURING_COMPAT_H
 #define LIBURING_COMPAT_H
 
+#if __has_include(<linux/time_types.h>)
 #include <linux/time_types.h>
+#else
+
+#ifndef __kernel_rwf_t
+#include <linux/types.h>
+typedef int __bitwise __kernel_rwf_t;
+#endif
+
+#ifndef __kernel_timespec
+struct __kernel_timespec {
+	long long tv_sec;
+	long long tv_nsec;
+};
+#endif
 /* <linux/time_types.h> is included above and not needed again */
 #define UAPI_LINUX_IO_URING_H_SKIP_LINUX_TIME_TYPES_H 1
+#endif
 
 #include <inttypes.h>
 
